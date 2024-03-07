97d1e7e696a5d969f70eb72bf3c8c56f6c1be734
jhwan <plasticat104@gmail.com>
2024-03-07 12:15:57 +0900

    feat: teacher analytics page

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -17,7 +17,7 @@ export const NavbarRoutes = () => {
   const isTeacherPage = pathname?.startsWith('/teacher')
   const isCoursePage = pathname?.includes('/courses')
   const isSearchPage = pathname === '/search'
-  console.log('userId:', userId)
+
   return (
     <>
       {isSearchPage && (
